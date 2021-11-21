5ae0e1b397637cf518de3ee5c7127da7d51d9016
Shirley Tatiana Diaz <shirley.diaz.mt@correo.usa.edu.co>
2021-11-21 15:45:43 -0500

    Frontend registro usuario

---
+++ frontend/js/User.js
@@ -0,0 +1,185 @@
+$(document).ready(function() {
+    checkUser() //Consulta de usuarios
+})
+
+function registerUser(){
+    let user = {
+        name: $("#user_name").val(),
+        email: $("#user_email").val(),
+        password: $("#user_password").val()
+    };
+    
+    if (validateUser(user)){
+        $.ajax({
+            url: url+"",
+            type: 'POST',
+            dataType: 'json',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            data: JSON.stringify(user),
+            statusCode:{
+                201:function(){
+                    alert('Se ha registrado el usuario');
+                    checkUser()
+                    cleanFieldsUser()
+                },
+                555:function(){
+                    alert('Ya existe un usuario con ese id')
+                }
+            }
+        });
+    }
+    
+    
+
+}
+
+
+function checkUser(){
+    $("#table_User").empty();
+    $.ajax({
+        url: url+ "",
+        type: 'GET',
+        dataType: 'json',
+        success: function(respuesta){
+            showUsers(respuesta);
+        },
+        error: function (xhr, status) {
+            alert('ha sucedido un problema');
+        }
+    });
+}
+
+function showUsers(items){
+    var tabla = `<table border="1">
+                  <tr>
+                    
+                    <th>Nombre</th>
+                    <th>Correo</th>
+                    
+                    
+                  </tr>`;
+                  
+    
+    for (var i=0; i < items.length; i++) {
+        tabla +=`<tr>
+                   <td>${items[i].name}</td>
+                   <td>${items[i].email}</td>
+                   <td>
+                        <a href="inicio.html?id=${items[i].idUser}">
+                   </td> 
+                </tr>`;
+    }
+    tabla +=`</table>`;
+
+    $("#table_User").html(tabla);
+}
+
+/**
+ * 
+ * @param {type} id
+ * @returns {undefined}
+ */
+function checkUserById(id){
+    $.ajax({
+        url: url+ ""+id,
+        type: 'GET',
+        dataType: 'json',
+        success: function(user){
+            console.log(user);
+            showUniqueUser(user);
+        },
+        error: function (xhr, status) {
+            alert('ha sucedido un problema');
+        }
+    });
+}
+/**
+ * 
+ * @param {User} item
+ * @returns {undefined}
+ */
+function showUniqueUser(item) {
+    $("#id_user").val(item.idUser);
+    $("#user_name").val(item.name);
+    $("#user_email").val(item.email);
+    
+}
+
+
+function updateUser(){
+
+    let user = {
+        idUser: $("#user_id").val(),
+        name: $("#user_name").val(),
+        email: $("#user_email").val(),
+        password: $("#user_password").val()
+    };
+    console.log(user);
+    if (validateUser(user)){
+        $.ajax({
+            url: url+"",
+            type: 'PUT',
+            dataType: 'json',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            data: JSON.stringify(user),
+            statusCode:{
+                201:function(){
+                    alert('Se han actualizado los datos del usuario');
+                    window.location.assign('inicio.html');
+                }
+            }
+        });
+
+        }
+}
+
+function removeUser(id){
+    let opc = confirm('¿Está seguro que desea eliminar a ese usuario?')
+    if(opc){
+        $.ajax({
+            url: url+""+id,
+            type: 'DELETE',
+            dataType: 'json',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            data: JSON.stringify({idUser:id}),
+            statusCode:{
+                204:function(){
+                    alert('Se ha eliminado el usuario');
+                    checkUser()
+                }
+            },
+        });
+    }
+
+}
+
+function validateUser(user){
+    if (user.id<=0|| user.name===''|| user.email==='' || user.password===''){
+        alert("Procure no dejar campos vacíos")
+        return false;
+    }else if (user.name.length>80){
+        alert('Contenido demasiado extenso. Solamente puede tener hasta 80 caracteres')
+        return false;
+    }
+    else if (user.password.length>50){
+        alert('Su contraseña es demasiado extensa. Solamente puede tener hasta 50 caracteres')
+        return false;
+    }
+    else if (user.email.length>50){
+        alert('Su correo es demasiado extenso. Solamente puede tener hasta 50 caracteres')
+        return false;
+    }
+    return true;
+}
+
+function cleanFieldsUser(){
+    $("#user_name").val('');
+    $("#user_email").val('');
+    $("#user_password").val('')
+}
