b3822e42fff8729060b3fce562ab8983b3fb63aa
srg77 <samuel-rodriguez2@upc.edu.co>
2021-11-29 06:43:15 -0500

    corrección visual y de evento llenar campos en pagina registroAdmin

--- frontend/js/UserAdmin.js
+++ frontend/js/UserAdmin.js
@@ -1,3 +1,4 @@
+var listaItems;//variable global que almacena la lista de usuarios
 function saveUser(){
     console.log("Ejecutando funcion para guardar");
     
@@ -29,7 +30,7 @@ function saveUser(){
             data: JSON.stringify(userAd),
             statusCode:{
                 201:function(){
-                    alert('Se ha registrado el producto');
+                    alert('Cuenta creada de forma correcta');
                     allUsers();
                     }
                 },
@@ -49,6 +50,7 @@ function allUsers(){
         dataType: 'json',
         success: function(respuesta){
             showUsers(respuesta);
+            listaItems = respuesta;
         },
         error: function (xhr, status) {
             alert('ha sucedido un problema');
@@ -57,60 +59,90 @@ function allUsers(){
 }
 
 function showUsers(items){
-    var tabla = `<table border="1">
-                  <tr>
-                    
-                    <th>Identificacion</th>
-                    <th>Nombre</th>
-                    <th>Direccion</th>
-                    <th>Celular</th>
-                    <th>Email</th>
-                    <th>Contraseña</th>
-                    <th>Zona</th>
-                    <th>Tipo</th>
-
-                  </tr>`;
-                  
+    var tabla = `<table class="table table-dark table-striped" border="1">
+                    <thead >
+                        <tr>
+                            <th>Identificacion</th>
+                            <th>Nombre</th>
+                            <th>Direccion</th>
+                            <th>Celular</th>
+                            <th>Email</th>
+                            <th>Contraseña</th>
+                            <th>Zona</th>
+                            <th>Tipo</th>
+                            <th>Opciones</th>
+                        </tr>
+                    </thead>`;
+                
+                
     
     for (var i=0; i < items.length; i++) {
-        let item= items;
         tabla +=`<tr>
-                   <td>${items[i].identification}</td>
-                   <td>${items[i].name}</td>
-                   <td>${items[i].address}</td>
-                   <td>${items[i].cellPhone}</td>
-                   <td>${items[i].email}</td>
-                   <td>${items[i].password}</td>
-                   <td>${items[i].zone}</td>
-                   <td>${items[i].type}</td>
-                   <td>
-                   <button onclick="updateUser(${item})">Actualizar</button>
-                   <button onclick="removeUser(${items[i].id})">Eliminar</button>
-                   </td> 
+                    <td>${items[i].identification}</td>
+                    <td>${items[i].name}</td>
+                    <td>${items[i].address}</td>
+                    <td>${items[i].cellPhone}</td>
+                    <td>${items[i].email}</td>
+                    <td>${items[i].password}</td>
+                    <td>${items[i].zone}</td>
+                    <td>${items[i].type}</td>
+                    <td>
+                        <button onclick="fillInformation(${items[i].id})" >Actualizar</button>`
+                        tabla+=`<button onclick="removeUser(${items[i].id})">Eliminar</button>
+                    </td> 
                 </tr>`;
     }
     tabla +=`</table>`;
 
     $("#table_UserAdmin").html(tabla);
-}
+    cleanFieldsUser();
 
-function updateUser(item){
-    alert("Hola entre update :)" + item.id );
-    $("#idprueba").val(item.id);
+}
+function fillInformation(id){
+
+    alert("A continuación modifique los campos que desea actualizar");
+    let infoItem;
+    for (const elemento of listaItems) {
+
+        if(elemento.id == id){
+            console.log(elemento.id)
+            infoItem = elemento;
+        }
+        
+    }
+    $("#idprueba").val(infoItem.id);
+    $("#user_identification").val(infoItem.identification);
+    $("#user_namec").val(infoItem.name);
+    $("#user_adress").val(infoItem.address);
+    $("#user_cellphone").val(infoItem.cellPhone);
+    $("#user_email").val(infoItem.email);
+    $("#user_password").val(infoItem.password);
+    $("#user_zone").val(infoItem.zone);
+    $("#user_type").val(infoItem.type);
+
+    
+    $("#btn-group").append("<button id=\"btn-cambios\" class=\"btn btn-primary text-center\" type=\"button\">Guardar Cambios</button>");
+    
+
     let userAd = {
         id: $("#idprueba").val(),
         identification: $("#user_identification").val(),
         name: $("#user_namec").val(),
-        adress: $("#user_adress").val(),
-        cellphone: $("#user_cellphone").val(),
+        address: $("#user_adress").val(),
+        cellPhone: $("#user_cellphone").val(),
         email: $("#user_email").val(),
         password: $("#user_password").val(),
         zone: $("#user_zone").val(),
         type: $("#user_type").val()
     };
+    console.log(userAd)
+    $("#btn-cambios").click(()=> updateUser(userAd));
+}
 
-    console.log(userAd);
-    if (validateUser(userAd)){
+function updateUser(userUpdate){
+
+    console.log(userUpdate);
+    if (validateUser(userUpdate)){
         $.ajax({
             url: url+"/api/user/update",
             type: 'PUT',
@@ -118,16 +150,17 @@ function updateUser(item){
             headers: {
                 "Content-Type": "application/json"
             },
-            data: JSON.stringify(userAd),
+            data: JSON.stringify(userUpdate),
             statusCode:{
                 201:function(){
-                    alert('Se han actualizado los datos del producto');
+                    alert('Usuario editado');
+                    allUsers();
                 }
             }
         });
 
     }else{
-        alert("No ha sido posible actualizar al usuario");
+        alert("Usuario no editado");
     }
 }
 
@@ -142,11 +175,22 @@ function removeUser(id){
         data: JSON.stringify({idUser:id}),
         statusCode:{
             204:function(){
-                alert('Se ha eliminado el Usuario');
+                alert('Usuario borrado de la BD');
                 allUsers()
             }
             },
         });
+}
+function cleanFieldsUser(){
+    $("#idprueba").val("");
+    $("#user_identification").val("");
+    $("#user_namec").val("");
+    $("#user_adress").val("");
+    $("#user_cellphone").val("");
+    $("#user_email").val("");
+    $("#user_password").val("");
+    $("#user_zone").val("");
+    $("#user_type").val("");
 }
 
 function validateUser(user){
