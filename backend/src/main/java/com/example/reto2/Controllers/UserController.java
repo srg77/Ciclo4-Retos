81054837e42e1080c9c6f693c60a43aaf5f483d8
srg77 <samuel-rodriguez2@upc.edu.co>
2021-12-01 19:06:37 -0500

    buenas practicas y funcion para eliminar info de la bd

--- backend/src/main/java/com/example/reto2/Controllers/UserController.java
+++ backend/src/main/java/com/example/reto2/Controllers/UserController.java
@@ -26,28 +26,46 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/user")
 @CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT,
         RequestMethod.DELETE })
+/**
+ * Clase controladora de rutas
+ */
 public class UserController {
 
 	@Autowired
+	/**
+	* instacia de servicio a usar
+	*/
 	private UserService service;
 
 	@GetMapping("/all")
+	/**
+	* método para listar todos los usuarios
+	*/
 	public List<User> getAll() {
 		return service.getUsers();
 	}
 
 	@ResponseStatus(HttpStatus.CREATED)
 	@PostMapping("/new")
+	/**
+	* método para agregar usuarios
+	*/
 	public User create(@RequestBody User usuario) {
 		return service.save(usuario);
 	}
 
 	@GetMapping("/emailexist/{email}")
+	/**
+	* método para validar email de usuario
+	*/
 	public boolean findEmail(@PathVariable("email") String userEmail) {
 		return service.getValidationEmail(userEmail);
 	}
 
 	@GetMapping("/{email}/{password}")
+	/**
+	* método para validar credenciales de usuario
+	*/
 	public User getAccess(@PathVariable("email") String userEmail, @PathVariable("password") String userPassword) {
 		return service.getValidationCredentials(userEmail, userPassword);
 	}
@@ -55,12 +73,18 @@ public class UserController {
 
 	@PutMapping("/update")
     @ResponseStatus(HttpStatus.CREATED)
+	/**
+	* método para actualizar un usuario
+	*/
     public User update(@RequestBody User usuario) {
         return service.save(usuario);
     }
 	
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+	/**
+	* método para eliminar usuario
+	*/
     public boolean delete(@PathVariable("id") Integer userId) {
         return service.deleteUser(userId);
     }
--- backend/src/main/java/com/example/reto2/Reto2Application.java
+++ backend/src/main/java/com/example/reto2/Reto2Application.java
@@ -1,13 +1,28 @@
 package com.example.reto2;
 
+import com.example.reto2.CrudRepository.ChocolateRepository;
+import com.example.reto2.CrudRepository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Reto2Application {
+public class Reto2Application  implements CommandLineRunner{
 
+	@Autowired
+    private ChocolateRepository interfaceChocolate;
+    @Autowired
+    private UserRepository interfaceUser;
 	public static void main(String[] args) {
 		SpringApplication.run(Reto2Application.class, args);
 	}
+
+	@Override
+    public void run(String... args) throws Exception {
+        interfaceChocolate.deleteAll();
+        interfaceUser.deleteAll();
+    }
 
 }
