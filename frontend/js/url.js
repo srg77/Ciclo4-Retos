424a5060d91293c2ae7488eb1d6a8e73bdbd158a
srg77 <samuel-rodriguez2@upc.edu.co>
2021-12-06 18:53:51 -0500

    actualizando url backend

--- frontend/js/url.js
+++ frontend/js/url.js
@@ -1,6 +1,6 @@
 //backend julian
 // var url = "http://129.151.121.220:8081"
 //backend samuel
-// var url ="http://152.70.141.56:8081"
+// var url ="http://152.70.141.56:8082"
 //backend local
 var url ="http://localhost:8082";
